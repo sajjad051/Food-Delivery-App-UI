d2e0b1bacce640020b22f23f19c55e60568cb801
Md. Sajjad Hossain <md.sajjad.0516@gmail.com>
2024-03-09 00:12:25 +0600

    4

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/widgets/app_bar_widget.dart';
+import 'package:food_delivery_app/widgets/drawer_widget.dart';
+import 'package:food_delivery_app/widgets/cart_bottom_nav_bar.dart';
+
+import 'package:flutter/cupertino.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -24,16 +28,349 @@ class CartPage extends StatelessWidget {
                     ),
                     child: Text(
                       "Order list",
-                      style: TextStyle(fontSize: 30,
-                      fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
-                  )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 9),
+                    child: Container(
+                      width: 380,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
+                            ),
+                          ]),
+                      child: Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              "images/Pizzaw.png",
+                              height: 80,
+                              width: 150,
+                            ),
+                          ),
+                          Container(
+                            width: 150,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  "Hot Pizza",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  "Teste Our Hot Pizza",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  "\$10",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(
+                                      CupertinoIcons.minus,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      "2",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              )),
+
+
+                        ],
+                      ),
+
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 9),
+                    child: Container(
+                      width: 380,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
+                            ),
+                          ]),
+                      child: Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              "images/drink.png",
+                              height: 80,
+                              width: 150,
+                            ),
+                          ),
+                          Container(
+                            width: 150,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  "Drink",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  "Teste Our Drink",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  "\$4",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(
+                                      CupertinoIcons.minus,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      "2",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              )),
+
+
+                        ],
+                      ),
+
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 9),
+                    child: Container(
+                      width: 380,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 3,
+                              blurRadius: 10,
+                              offset: Offset(0, 3),
+                            ),
+                          ]),
+                      child: Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: Image.asset(
+                              "images/Pizzaw.png",
+                              height: 80,
+                              width: 150,
+                            ),
+                          ),
+                          Container(
+                            width: 150,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text(
+                                  "Hot Pizza",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  "Teste Our Hot Pizza",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  "\$10",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                              padding: EdgeInsets.symmetric(vertical: 8),
+                              child: Container(
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      "2",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 15),
+                                    ),
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              )),
+
+
+                        ],
+                      ),
+
+                    ),
+                  ),
+
+      Padding(
+        padding: EdgeInsets.symmetric(vertical: 30,horizontal: 20),
+        child: Container(
+         padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 3,
+                  blurRadius: 10,
+                  offset: Offset(0, 3),
+                ),
+              ]),
+          child: Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Items:",style: TextStyle(fontSize: 20),),
+                  Text("\$10",style: TextStyle(fontSize: 20),),
+                ],
+              ),),
+              Divider(color: Colors.black,),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Sub-total:",style: TextStyle(fontSize: 20),),
+                    Text("\$60",style: TextStyle(fontSize: 20),),
+                  ],
+                ),),
+              Divider(color: Colors.black12,),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Delivery:",style: TextStyle(fontSize: 20),),
+                    Text("\$20",style: TextStyle(fontSize: 20),),
+                  ],
+                ),),
+                Divider(color: Colors.black12,),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Total:",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                    Text("\$80",style: TextStyle(fontSize: 20,color: Colors.red),),
+                  ],
+                ),),
+            ],
+          ),
+        ),
+      ),
                 ],
               ),
             ),
           )
         ],
       ),
+    drawer: DrawerWidget(),
+      bottomNavigationBar: CartBottomNavBar(),
     );
   }
 }
---
+++ lib/widgets/cart_bottom_nav_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CartBottomNavBar extends StatelessWidget {
+  const CartBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        height: 70,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Total",
+                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  "\$80",
+                  style: TextStyle(fontSize: 19, color: Colors.red),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )
+                )
+              ),
+              child: Text("Order Now"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
